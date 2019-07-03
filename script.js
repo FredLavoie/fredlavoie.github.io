f8340ecba4492050d1cc779a8c539750bc16e02f
FredLavoie <flavoie3@gmail.com>
2019-07-03 11:06:29 -0400

    Fix now and project display behaviour

--- script.js
+++ script.js
@@ -2,16 +2,17 @@ $(document).ready(function() {
 
   $('#project-content').css({'display': 'none'});
 
+
   // toggle 'now content' show/hide
   $('#now-button').on('click', function () {
     $('#now-content').slideToggle(300);
-    $('#project-content').slideToggle(300);
+    $('#project-content').css({'display': 'none'});
   });
 
     // toggle 'now content' show/hide
     $('#project-button').on('click', function () {
       $('#project-content').slideToggle(300);
-      $('#now-content').slideToggle(300);
+      $('#now-content').css({'display': 'none'});
     });
 
 });
