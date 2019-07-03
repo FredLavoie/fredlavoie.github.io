2349bb09b71168e320ab46fd17d19087e7fb5144
FredLavoie <flavoie3@gmail.com>
2019-07-03 09:25:55 -0400

    Add project page with flexbox

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 $(document).ready(function() {
 
+  $('#project-content').css({'display': 'none'});
+
   // toggle 'now content' show/hide
   $('#now-button').on('click', function () {
     $('#now-content').slideToggle(300);
+    $('#project-content').slideToggle(300);
   });
+
+    // toggle 'now content' show/hide
+    $('#project-button').on('click', function () {
+      $('#project-content').slideToggle(300);
+      $('#now-content').slideToggle(300);
+    });
 
 });
