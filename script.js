e607ff366fed20168f67569b0d99efc17b2eb2bb
FredLavoie <flavoie3@gmail.com>
2019-09-01 23:28:44 -0400

    Update nav behaviour
    - add background color of current displayed page
    - revise screenshot of concrete beam app

--- script.js
+++ script.js
@@ -1,17 +1,24 @@
 $(document).ready(function() {
 
+  $('#now-button').css({'background-color': '#4a148c', 'color': '#f4edf5'});
   $('#project-content').css({'display': 'none'});
 
   // toggle 'now content' show/hide
   $('#now-button').on('click', function () {
     $('#project-content').slideUp(400);
     $('#now-content').slideToggle(400);
+    $('#now-button').css({'background-color': '#4a148c', 'color': '#f4edf5'});
+    $('#project-button').css({'background-color': '', 'color': ''});
   });
 
   // toggle 'project content' show/hide
   $('#project-button').on('click', function () {
     $('#now-content').slideUp(400);
     $('#project-content').slideToggle(400);
+    $('#now-button').css({'background-color': '', 'color': ''});
+    $('#project-button').css({'background-color': '#4a148c', 'color': '#f4edf5'});
   });
+
+
 
 });
