f1efa1d4b7bb467d5a04c77d8a620e63d4a43aa4
FredLavoie <flavoie3@gmail.com>
2020-03-07 12:07:34 -0500

    Change to vanilla javascript

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,18 @@
-$(document).ready(function() {
+document.addEventListener('DOMContentLoaded', function(){
 
-	$('#menu-items').hide();
-
-	$('#menu').on('click', function() {
-	  $('#menu-items').toggle();
-	});
+	const menuItems = document.querySelector('#menu-items');
+	const menuButton = document.querySelector('#menu');
+	
+	// hide menu items on load
+	menuItems.style.display = 'none';
+	
+	// toggle show/hide menu on mobile
+	menuButton.onclick = function() {
+		if(menuItems.style.display === 'none') {
+			menuItems.style.display = 'flex';
+		} else if(menuItems.style.display === 'flex') {
+			menuItems.style.display = 'none';
+		}
+	}
 
 });
