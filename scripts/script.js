a63e3af2dc0d20965ff8b64a7c57b4553caf8b22
FredLavoie <flavoie3@gmail.com>
2020-03-07 12:34:35 -0500

    Add main header link to index + fix indentation

--- scripts/script.js
+++ scripts/script.js
@@ -1,18 +1,18 @@
 document.addEventListener('DOMContentLoaded', function(){
 
-	const menuItems = document.querySelector('#menu-items');
-	const menuButton = document.querySelector('#menu');
-	
-	// hide menu items on load
-	menuItems.style.display = 'none';
-	
-	// toggle show/hide menu on mobile
-	menuButton.onclick = function() {
-		if(menuItems.style.display === 'none') {
-			menuItems.style.display = 'flex';
-		} else if(menuItems.style.display === 'flex') {
-			menuItems.style.display = 'none';
-		}
-	}
+  const menuItems = document.querySelector('#menu-items');
+  const menuButton = document.querySelector('#menu');
+  
+  // hide menu items on load
+  menuItems.style.display = 'none';
+  
+  // toggle show/hide menu on mobile
+  menuButton.onclick = function() {
+    if(menuItems.style.display === 'none') {
+      menuItems.style.display = 'flex';
+    } else if(menuItems.style.display === 'flex') {
+      menuItems.style.display = 'none';
+    }
+  }
 
 });
